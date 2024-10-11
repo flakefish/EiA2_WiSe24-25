bfa948f7f1f5ca85951f5ad11f05b8e514d59f89
flakefish <lukas.perschau@gmail.com>
2024-10-11 13:05:00 +0200

    Linking Test

--- L01_Recap&Foundation/A01_RandomPoem/poet.ts
+++ L01_Recap&Foundation/A01_RandomPoem/poet.ts
@@ -1,1 +1,23 @@
-Expecto Patronum
+
+namespace zufallsgedicht {
+
+    let subject: string[] = ["Artemis", "Hades", "Zagreus", "Cerberus", "Persephone", "Poseidon"];
+    let predicate: string[] = ["jagt", "hasst", "kuschelt", "füttert", "liebt", "sucht"];
+    let object: string[] = ["einen Wolf", "Hunde", "Katzen", "Schneeleoparden", " große Elefanten", "Robben"];
+    
+    for (let i = subject.length; i >= 1; i--){
+        let wort = console.log(getVerse(subject, predicate, object));
+
+    }
+
+    function getVerse(_subject: string[], _predicate: string [], _object: string[]){
+        let verse: string = "";
+        let randomNumber1: number = Math.floor(Math.random()*subject.length);
+        let randomNumber2: number = Math.floor(Math.random()*predicate.length);
+        let randomNumber3: number = Math.floor(Math.random()*object.length);
+        verse += subject.splice(randomNumber1, 1)[0] + " " + predicate.splice(randomNumber2, 1)[0] + " " + object.splice(randomNumber3, 1)[0];
+        return verse;
+        console.log(verse);
+    }
+    
+    }
