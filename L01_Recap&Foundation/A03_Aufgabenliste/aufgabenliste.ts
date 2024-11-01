9eb7516ba03183fe366752c3cab10d361f2bda09
flakefish <lukas.perschau@gmail.com>
2024-11-01 22:12:38 +0100

    Aufgabe 3

---
+++ L01_Recap&Foundation/A03_Aufgabenliste/aufgabenliste.ts
@@ -0,0 +1,39 @@
+namespace Shoppinglist{   
+    document.addEventListener("DOMContentLoaded", () => {
+        const taskInput = document.getElementById("task");
+        const timeInput = document.getElementById("time");
+        const dateInput = document.getElementById("date");
+        const commentsInput = document.getElementById("comments");
+        const checkInput = document.getElementById("check");
+    
+        if (taskInput) {
+            taskInput.addEventListener("check", () => {
+                console.log("Aufgabe wurde hinzugefügt");
+            });
+        }
+    
+        if (timeInput) {
+            timeInput.addEventListener("change", () => {
+                console.log("Uhrzeit wurde verändert");
+            });
+        }
+    
+        if (dateInput) {
+            dateInput.addEventListener("change", () => {
+                console.log("Datum wurde verändert");
+            });
+        }
+    
+        if (commentsInput) {
+            commentsInput.addEventListener("input", () => {
+                console.log("Kommentar wurde hinzugefügt");
+            });
+        }
+    
+        if (checkInput) {
+            checkInput.addEventListener("change", () => {
+                console.log("Die Aufgabe soll gekauft werden");
+            });
+        }
+    });
+    }
