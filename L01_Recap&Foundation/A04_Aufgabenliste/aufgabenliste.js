d4caebf95403a85e1e886e6ac349457c5f9fc84b
flakefish <lukas.perschau@gmail.com>
2024-11-08 19:36:46 +0100

    Start Aufgabe 4 (+add task button)

---
+++ L01_Recap&Foundation/A04_Aufgabenliste/aufgabenliste.js
@@ -0,0 +1,37 @@
+"use strict";
+var Shoppinglist;
+(function (Shoppinglist) {
+    document.addEventListener("DOMContentLoaded", () => {
+        const taskInput = document.getElementById("task");
+        const timeInput = document.getElementById("time");
+        const dateInput = document.getElementById("date");
+        const commentsInput = document.getElementById("comments");
+        const checkInput = document.getElementById("check");
+        if (taskInput) {
+            taskInput.addEventListener("check", () => {
+                console.log("Aufgabe wurde hinzugefügt");
+            });
+        }
+        if (timeInput) {
+            timeInput.addEventListener("change", () => {
+                console.log("Uhrzeit wurde verändert");
+            });
+        }
+        if (dateInput) {
+            dateInput.addEventListener("change", () => {
+                console.log("Datum wurde verändert");
+            });
+        }
+        if (commentsInput) {
+            commentsInput.addEventListener("input", () => {
+                console.log("Kommentar wurde hinzugefügt");
+            });
+        }
+        if (checkInput) {
+            checkInput.addEventListener("change", () => {
+                console.log("Die Aufgabe soll gekauft werden");
+            });
+        }
+    });
+})(Shoppinglist || (Shoppinglist = {}));
+//# sourceMappingURL=aufgabenliste.js.map
---
+++ L01_Recap&Foundation/A04_Aufgabenliste/aufgabenliste.ts
@@ -0,0 +1,39 @@
+namespace Shoppinglist{   
+    document.addEventListener("DOMContentLoaded", () => {
+        const taskInput = document.getElementById("task");
+        const timeInput = document.getElementById("time");
+        const dateInput = document.getElementById("date");
+        const commentsInput = document.getElementById("comments");
+        const checkInput = document.getElementById("check");
+    
+        if (taskInput) {
+            taskInput.addEventListener("check", () => {
+                console.log("Aufgabe wurde hinzugefügt");
+            });
+        }
+    
+        if (timeInput) {
+            timeInput.addEventListener("change", () => {
+                console.log("Uhrzeit wurde verändert");
+            });
+        }
+    
+        if (dateInput) {
+            dateInput.addEventListener("change", () => {
+                console.log("Datum wurde verändert");
+            });
+        }
+    
+        if (commentsInput) {
+            commentsInput.addEventListener("input", () => {
+                console.log("Kommentar wurde hinzugefügt");
+            });
+        }
+    
+        if (checkInput) {
+            checkInput.addEventListener("change", () => {
+                console.log("Die Aufgabe soll erfüllt werden");
+            });
+        }
+    });
+    }
